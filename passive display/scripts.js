de431476e52dc68a2704525d060c28f0c39f3cd2
tylerjnorton <tylernorton@Tylers-MacBook-Pro.local>
2016-07-08 13:46:17 -0400

    longer list

--- passive display/scripts.js
+++ passive display/scripts.js
@@ -16,23 +16,39 @@ setInterval(function () {
 
 
 var stuffToShow = [
-	{ news:"Your app has updated", time:"2 days ago" },
-	{ news:"Your app has updated", time:"3 days ago" },
-	{ news:"Your app has updated", time:"4 days ago" },
-	{ news:"Your app has updated", time:"5 days ago" },
-	{ news:"Your app has updated", time:"6 days ago" },
-	{ news:"Your app has updated", time:"7 days ago" },
-	{ news:"Your app has updated", time:"8 days ago" },
-	{ news:"Your app has updated", time:"9 days ago" },
-	{ news:"Your app has updated", time:"10 days ago" },
-	{ news:"Your app has updated", time:"11 days ago" },
-	{ news:"Your app has updated", time:"12 days ago" },
-	{ news:"Your app has updated", time:"13 days ago" }
+	{ news:"Your apple has updated", time:"2 days ago" },
+	{ news:"Your banana has updated", time:"3 days ago" },
+	{ news:"Your carrot has updated", time:"4 days ago" },
+	{ news:"Your dumpling has updated", time:"5 days ago" },
+	{ news:"Your egret has updated", time:"6 days ago" },
+	{ news:"Your ferret has updated", time:"7 days ago" },
+	{ news:"Your grapes have updated", time:"8 days ago" },
+	{ news:"Your hippo has updated", time:"9 days ago" },
+	{ news:"Your indie band has updated", time:"10 days ago" },
+	{ news:"Your juice has updated", time:"11 days ago" },
+	{ news:"Your kowala has updated", time:"12 days ago" },
+	{ news:"Your lemon tree has updated", time:"14 days ago" },
+	{ news:"Your minecraft has updated", time:"15 days ago" },
+	{ news:"Your norton antivirus have updated", time:"16 days ago" },
+	{ news:"Your octopus has updated", time:"17 days ago" },
+	{ news:"Your puddle of mudd band has updated", time:"18 days ago" },
+	{ news:"Your queen album has updated", time:"19 days ago" },
+	{ news:"Your reddit thread has updated", time:"20 days ago" },
+	{ news:"Your stapler tree has updated", time:"21 days ago" },
+	{ news:"Your tractor has updated", time:"22 days ago" },
+	{ news:"Your underpants band has updated", time:"23 days ago" },
+	{ news:"Your wiggle room has updated", time:"24 days ago" },
+	{ news:"Your x-ray vision has updated", time:"25 days ago" },
+	{ news:"Your yoyo vision has updated", time:"26 days ago" },
+	{ news:"Your zebra tree has updated", time:"27 days ago" }
 ];
 
+
 var msg = `${stuffToShow[0].news} <span class="time"> ${stuffToShow[0].time}</span>`;
+
 var int;
 for (int = 0; int < items.length; int++) {
     itemsA[int].innerHTML = msg;
     items[int].innerHTML = msg;
 };
+
