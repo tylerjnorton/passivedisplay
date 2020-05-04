d5fbdfc839f252a12f0cf97dea46ca1a1a198f1d
Adam <adamcoll.ac@gmail.com>
2020-05-04 15:23:56 -0700

    clean up video stuff

--- src/scripts.js
+++ src/scripts.js
@@ -1,82 +1,105 @@
-var amScrolling = false;
-const sections = document.querySelectorAll('section');
-var SOUND = false;
+const sections = document.querySelectorAll("section");
+const PLAY_DOPE_SOUND_EFFEX = false;
 
-var NOTIFICATIONS = [];
+let NOTIFICATIONS = [];
 
 function refreshNotifications() {
-	return fetch('https://adamcoll-passive-display.builtwithdark.com/items')
-	.then(res => res.json())
-	.then(data => { 
-		NOTIFICATIONS = data.map(item => ({...item, lastUpdated: new Date(item.lastUpdated), time: new Date(item.time)})); 
-	});
+  return fetch("https://adamcoll-passive-display.builtwithdark.com/items")
+    .then((res) => res.json())
+    .then((data) => {
+      NOTIFICATIONS = data.map((item) => ({
+        ...item,
+        lastUpdated: new Date(item.lastUpdated),
+        time: new Date(item.time),
+      }));
+    });
 }
 
-var currentIndex = 0;
+let CURRENT_ROTATION_INDEX = 0;
 function takeNextItems(theArray, howManyToTake) {
-	const results = [];
-	while (results.length < howManyToTake && results.length <= theArray.length) {
-		if (currentIndex > theArray.length - 1) {
-			currentIndex = 0;
-		}
-		results.push(theArray[currentIndex]);
-		currentIndex++;
-	}
-	return results
-}
-
-function showThemSuckas () {
-
-	const pinnedItems = NOTIFICATIONS.filter(item => item.pinned);
-	const howManyMoreWeNeed = sections.length - pinnedItems.length;
-	const dynamicItems = takeNextItems(NOTIFICATIONS.filter(item => !item.pinned), howManyMoreWeNeed).sort((a,b) => a.time.getTime() - b.time.getTime());
-
-	for (let i = 0 ; i < sections.length; i++) {
-		setTimeout( function () {
-			const activeItem = sections[i].querySelector(".item.active");
-			const inactiveItem = sections[i].querySelector(".item:not(.active)");
-			const next = pinnedItems[i] || dynamicItems[i - pinnedItems.length];
-
-			if (next && next.pinned) {
-				sections[i].classList.add('pinned');
-			} else {
-				sections[i].classList.remove('pinned');
-			}
-
-			inactiveItem.innerHTML =  next ? `<div class="news">${next.news}</div> <div class="time"> Last Updated<br />${moment(next.lastUpdated).fromNow()}</div>` : '';
-
-			// Flip	
-			inactiveItem.classList.toggle('active');
-			activeItem.classList.toggle('active');
-		
-
-			if (SOUND) {
-				document.getElementById("flip"+i).play();
-			}
-		}, 150*i);
-	}
-}
-var Util={};
-Util.base64 = function(mimeType, base64) {
-  return 'data:' + mimeType + ';base64,' + base64;
-};
-
-var video = document.createElement('video');
-  video.setAttribute('loop', '');
-
-  function addSourceToVideo(element, type, dataURI) {
-    var source = document.createElement('source');
-    source.src = dataURI;
-    source.type = 'video/' + type;
-    element.appendChild(source);
+  const results = [];
+  while (results.length < howManyToTake && results.length <= theArray.length) {
+    if (CURRENT_ROTATION_INDEX > theArray.length - 1) {
+      CURRENT_ROTATION_INDEX = 0;
+    }
+    results.push(theArray[CURRENT_ROTATION_INDEX]);
+    CURRENT_ROTATION_INDEX++;
   }
+  return results;
+}
 
-  addSourceToVideo(video,'webm', Util.base64('video/webm', 'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='));
-  addSourceToVideo(video, 'mp4', Util.base64('video/mp4', 'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='));
+function flipDemSuckas() {
+  const pinnedItems = NOTIFICATIONS.filter((item) => item.pinned);
+  const howManyMoreWeNeed = sections.length - pinnedItems.length;
+  const dynamicItems = takeNextItems(
+    NOTIFICATIONS.filter((item) => !item.pinned),
+    howManyMoreWeNeed
+  ).sort((a, b) => a.time.getTime() - b.time.getTime());
 
-video.play();
+  for (let i = 0; i < sections.length; i++) {
+    setTimeout(function () {
+      const activeItem = sections[i].querySelector(".item.active");
+      const inactiveItem = sections[i].querySelector(".item:not(.active)");
+      const next = pinnedItems[i] || dynamicItems[i - pinnedItems.length];
 
-// Run extra 5 seconds
-setInterval(refreshNotifications, 120000);
-setInterval(showThemSuckas, 30000);
-refreshNotifications().then(() => showThemSuckas());
+      if (next && next.pinned) {
+        sections[i].classList.add("pinned");
+      } else {
+        sections[i].classList.remove("pinned");
+      }
+
+      inactiveItem.innerHTML = next
+        ? `<div class="news">${
+            next.news
+          }</div> <div class="time"> Last Updated<br />${moment(
+            next.lastUpdated
+          ).fromNow()}</div>`
+        : "";
+
+      // Flip
+      if (activeItem.innerHTML !== inactiveItem.innerHTML) {
+        inactiveItem.classList.toggle("active");
+        activeItem.classList.toggle("active");
+      }
+
+      if (PLAY_DOPE_SOUND_EFFEX) {
+        document.getElementById("flip" + i).play();
+      }
+    }, 150 * i);
+  }
+}
+
+const video = document.createElement("video");
+video.setAttribute("loop", "");
+
+function addSourceToVideo(element, type, dataURI) {
+  var source = document.createElement("source");
+  source.src = "data:video" + type + ";base64," + dataURI;
+  source.type = "video/" + type;
+  element.appendChild(source);
+}
+
+addSourceToVideo(
+  video,
+  "webm",
+  "GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA="
+);
+addSourceToVideo(
+  video,
+  "mp4",
+  "AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=="
+);
+
+const ONE_MINUTE = 1000 /* milliseconds */ * 60; /* seconds */
+
+setInterval(refreshNotifications, ONE_MINUTE * 2);
+setInterval(flipDemSuckas, ONE_MINUTE * 0.5);
+
+// fetch data on load and immediately flip
+refreshNotifications().then(() => {
+  flipDemSuckas();
+
+  // chrome sometimes prevents this because the user didn't interact with the page before its called...
+  // Maybe on first load we can make the user click something..
+  video.play();
+});
